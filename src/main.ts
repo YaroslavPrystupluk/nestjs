792da46d1d9cfe037e11edb6d1f42b7735a793ff
Yaroslav Prystuplyuk <tyucha_05@ukr.net>
2026-09-16 18:06:51 +0300

    add config to app

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+  config.getOrThrow('POSTGRES_USER');
 
   app.use(cookieParser());
   app.setGlobalPrefix('api');
