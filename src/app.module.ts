ce661d9391ad3160ce29172aa5b49771d2c71e47
Yaroslav Prystuplyuk <tyucha_05@ukr.net>
2026-09-17 15:05:39 +0300

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AppController } from './app.controller.js';
 import { SpotifyModule } from './spotify/spotify.module.js';
+import { FileModule } from './file/file.module.js';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { SpotifyModule } from './spotify/spotify.module.js';
     }),
     PrismaModule,
     SpotifyModule,
+    FileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
